95760eb3778889e493bea9e49a9ff729b6c15e27
sbx_user1051 <sbx_user1051@169.254.55.37>
2024-05-09 13:00:53 +0000

    Thu May 09 13:00:53 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,8 +2,84 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.*;
+
 public class CheckoutSolution {
+
+    //while we could use a map, at this size, it is faster to use a list (for now)
+    List<Item> itemList = List.of(
+            new Item("A", 50, new SpecialOffer(3, 130)),
+            new Item("B", 30, new SpecialOffer(2, 45)),
+            new Item("C", 20),
+            new Item("D", 15)
+    );
+
+    /*
+    notes/questions: are skus multiples of the same letter or with numerals, i.e. 3A or AAA?
+     */
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        String[] individualSkus = skus.split("");
+        Map<Item, Integer> cartItems = new HashMap<>();
+        for (String sku : individualSkus) {
+            Optional<Item> itemOpt = itemList.stream()
+                    .filter(item -> Objects.equals(item.sku(), sku))
+                    .findFirst();
+            if (itemOpt.isEmpty()) {
+                return -1;
+            }
+            cartItems.compute(
+                    itemOpt.get(),
+                    (item, integer) -> integer == null ? 1 : integer + 1
+            );
+        }
+
+        Integer totalSum = 0;
+        for (var skuEntry : cartItems.entrySet()) {
+            /*
+            for a regular item, the price is item price * quantity
+            for an item with a special offer, the price is
+                quantity divided by special offer amount (e.g. for 7 A, 7 / 3 = 2.333, int round down to 2
+                special offer value = 2 * special offer value
+                remainder items 7 % 3 = 1, 1 * price
+                total 2*special offer + 1*price
+             */
+            Item item = skuEntry.getKey();
+            Integer itemQuantity = skuEntry.getValue();
+            int amountOfSpecialOffers = 0;
+            if (item.hasSpecialOffer()) {
+                amountOfSpecialOffers = itemQuantity / item.specialOffer().quantity(); //int downcasting works in our favour here
+                int remainingItems = itemQuantity % item.specialOffer().quantity();
+                totalSum += (item.specialOffer().price() * amountOfSpecialOffers) + (item.price() * remainingItems);
+            } else {
+                totalSum += itemQuantity * item.price();
+            }
+        }
+        return totalSum;
+    }
+
+    record Item(
+            String sku,
+            Integer price,
+            SpecialOffer specialOffer
+    ) {
+
+        public Item(
+                String sku,
+                Integer price
+        ) {
+            this(sku, price, null);
+        }
+
+        public boolean hasSpecialOffer() {
+            return specialOffer != null;
+        }
+
+    }
+
+    record SpecialOffer(
+            Integer quantity,
+            Integer price
+    ) {
+
     }
 }
--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -1,4 +1,51 @@
+package befaster.solutions.CHK;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CheckoutSolutionTest {
-  
+
+    private final CheckoutSolution checkoutSolution = new CheckoutSolution();
+
+    @Nested
+    class ValidSolutions {
+
+        @Test
+        void valid_skus() {
+            int actualPrice = checkoutSolution.checkout("ABCD");
+            assertEquals(115, actualPrice);
+        }
+
+        @Test
+        void valid_skus_with_offer() {
+            int actualPrice = checkoutSolution.checkout("AAA");
+            assertEquals(130, actualPrice);
+        }
+
+        @Test
+        void valid_skus_with_multiple_offers() {
+            int actualPrice = checkoutSolution.checkout("AAABB");
+            assertEquals(175, actualPrice);
+        }
+    }
+
+    @Nested
+    class InvalidSolutions {
+
+        @Test
+        void invalid_skus() {
+            int actualPrice = checkoutSolution.checkout("E");
+            assertEquals(-1, actualPrice);
+        }
+
+        @Test
+        void invalid_skus_mixed_with_valid_skus() {
+            int actualPrice = checkoutSolution.checkout("ABCDE");
+            assertEquals(-1, actualPrice);
+        }
+
+    }
+
 }
